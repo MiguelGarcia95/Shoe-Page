c737905180f66fda38f4d7075f3805a806fb7e84
MiguelGarcia95 <mgarcia95951@gmail.com>
2019-09-08 03:26:01 -0500

    animate page

--- js/main.js
+++ js/main.js
@@ -1,3 +1,4 @@
+var loadingPage = new TimelineMax();
 var menuEnters = new TimelineMax({paused: true});
 var menuLeaves = new TimelineMax({paused: true});
 var shoeO1BlackEnters = new TimelineMax({paused: true});
@@ -6,17 +7,23 @@ var shoeO1PinkEnters = new TimelineMax({paused: true});
 var shoeO1BlackLeaves = new TimelineMax({paused: true});
 var shoeO1WhiteLeaves = new TimelineMax({paused: true});
 var shoeO1PinkLeaves = new TimelineMax({paused: true});
-
 var shoeO2BlackEnters = new TimelineMax({paused: true});
 var shoeO2WhiteEnters = new TimelineMax({paused: true});
 var shoeO2YellowEnters = new TimelineMax({paused: true});
 var shoeO2BlackLeaves = new TimelineMax({paused: true});
 var shoeO2WhiteLeaves = new TimelineMax({paused: true});
 var shoeO2YellowLeaves = new TimelineMax({paused: true});
-
 var shoeO3BlackEnters = new TimelineMax({paused: true});
 var shoeO3WhiteEnters = new TimelineMax({paused: true});
 var shoeO3BlueEnters = new TimelineMax({paused: true});
 var shoeO3BlackLeaves = new TimelineMax({paused: true});
 var shoeO3WhiteLeaves = new TimelineMax({paused: true});
 var shoeO3BlueLeaves = new TimelineMax({paused: true});
+
+
+function start() {
+  loadingPage.pause();
+  console.log('runs');
+}
+
+window.onload = start();

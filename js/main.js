6caf7fc35725623070d23388a9d1ba8cdbe80413
MiguelGarcia95 <mgarcia95951@gmail.com>
2019-09-08 20:38:00 -0500

    animate page

--- js/main.js
+++ js/main.js
@@ -41,6 +41,23 @@ goToShoeColor('shoe_01_white', 'shoe_01', 'black');
 goToShoeColor('shoe_01_pink', 'shoe_01', 'black');
 goToShoeColor('shoe_01_pink', 'shoe_01', 'white');
 
+// Colors For Shoe 02
+goToShoeColor('shoe_02_black', 'shoe_02', 'yellow');
+goToShoeColor('shoe_02_black', 'shoe_02', 'pink');
+goToShoeColor('shoe_02_yellow', 'shoe_02', 'pink');
+goToShoeColor('shoe_02_yellow', 'shoe_02', 'black');
+goToShoeColor('shoe_02_pink', 'shoe_02', 'black');
+goToShoeColor('shoe_02_pink', 'shoe_02', 'yellow');
+
+// Colors For Shoe 02
+goToShoeColor('shoe_03_black', 'shoe_03', 'white');
+goToShoeColor('shoe_03_black', 'shoe_03', 'blue');
+goToShoeColor('shoe_03_blue', 'shoe_03', 'white');
+goToShoeColor('shoe_03_blue', 'shoe_03', 'black');
+goToShoeColor('shoe_03_white', 'shoe_03', 'black');
+goToShoeColor('shoe_03_white', 'shoe_03', 'blue');
+
+
 // Go To Shoe from menu
 function goToShoe(idName) {
   document.getElementById(idName).addEventListener('click', function() {
@@ -63,7 +80,12 @@ goToMenu('.shoe_01_black');
 goToMenu('.shoe_01_white');
 goToMenu('.shoe_01_pink');
 goToMenu('.shoe_02_yellow');
+goToMenu('.shoe_02_pink');
+goToMenu('.shoe_02_black');
+
 goToMenu('.shoe_03_blue');
+goToMenu('.shoe_03_black');
+goToMenu('.shoe_03_white');
 
 
 function onShoeClick (tl, className) {

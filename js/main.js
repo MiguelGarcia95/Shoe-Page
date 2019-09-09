9516ab4b0081a2ac8b8efd9fef047a92e1352ccf
MiguelGarcia95 <mgarcia95951@gmail.com>
2019-09-08 20:02:07 -0500

    animate page

--- js/main.js
+++ js/main.js
@@ -4,32 +4,12 @@ var menuEnters = new TimelineMax({paused: true});
 var menuLeaves = new TimelineMax({paused: true});
 
 var shoeEnters = new TimelineMax({paused: true});
-// var shoeO1BlackEnters = new TimelineMax({paused: true});
-// var shoeO1WhiteEnters = new TimelineMax({paused: true});
-// var shoeO1PinkEnters = new TimelineMax({paused: true});
-// var shoeO1BlackLeaves = new TimelineMax({paused: true});
-// var shoeO1WhiteLeaves = new TimelineMax({paused: true});
-// var shoeO1PinkLeaves = new TimelineMax({paused: true});
-var shoeO2BlackEnters = new TimelineMax({paused: true});
-var shoeO2WhiteEnters = new TimelineMax({paused: true});
-var shoeO2YellowEnters = new TimelineMax({paused: true});
-var shoeO2BlackLeaves = new TimelineMax({paused: true});
-var shoeO2WhiteLeaves = new TimelineMax({paused: true});
-var shoeO2YellowLeaves = new TimelineMax({paused: true});
-var shoeO3BlackEnters = new TimelineMax({paused: true});
-var shoeO3WhiteEnters = new TimelineMax({paused: true});
-var shoeO3BlueEnters = new TimelineMax({paused: true});
-var shoeO3BlackLeaves = new TimelineMax({paused: true});
-var shoeO3WhiteLeaves = new TimelineMax({paused: true});
-var shoeO3BlueLeaves = new TimelineMax({paused: true});
+var shoeLeaves = new TimelineMax({paused: true});
 
 loadingPage.to('.loading', 0.5, {scale: 100, autoAlpha: 0});
 
 menuEnters.fromTo('.menu', 1, {scale: 0, autoAlpha: 0}, {display: 'block', autoAlpha: 1, scale: 1, ease: Elastic.easeInOut.config(1, 0.3)});
 menuLeaves.fromTo('.menu', 2, {scale: 1}, {scale: 10, autoAlpha: 1, transformOrigin: 'center center', ease: Elastic.easeInOut.config(1, 0.3)});
-
-// shoeO1BlackEnters
-// .fromTo('.shoe_01_black', 1, {scale: 0, autoAlpha: 0}, {display: 'block', autoAlpha: 1, scale: 1, ease: Elastic.easeInOut.config(1, 0.3)})
 
 function start() {
   loadingPage.play();
